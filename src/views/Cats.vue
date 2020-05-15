a2e380d62129164a9447ee02143c018a437e1413
Dennis <dennislammers@live.nl>
2020-05-15 15:19:05 +0200

    Mid-tut commit for formatting :)

---
+++ src/views/Cats.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Cats for adoption</h1>
+    <b-table :items="cats" hover striped>
+      <template v-slot:cell(name)="data">
+        <router-link :to="`/pets/cats/${data.index}`">
+          {{ data.value }}
+        </router-link>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+  import {mapState} from 'vuex';
+
+  export default {
+    name: 'Cats',
+    data() {
+      return {};
+    },
+    computed: {
+      ...mapState(['cats']),
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Dogs.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Dogs for adoption</h1>
+    <b-table :items="dogs" hover striped>
+      <template v-slot:cell(name)="data">
+        <router-link :to="`/pets/dogs/${data.index}`">
+          {{ data.value }}
+        </router-link>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+  import {mapState} from 'vuex';
+
+  export default {
+    name: 'Dogs',
+    data() {
+      return {};
+    },
+    computed: {
+      ...mapState(['dogs']),
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
