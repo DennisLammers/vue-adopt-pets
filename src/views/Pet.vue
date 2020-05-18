f62a76e2c634b7981c7b5acf8d1e2468cb79a18c
Dennis <dennislammers@live.nl>
2020-05-18 08:51:13 +0200

    End of KC day

--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
-    {{ route.params.species }}
-    {{ route.params.id }}
+    <h1>{{animal.name}} ({{this.$route.params.species}})</h1>
+    <p>Age: {{animal.age}} years old</p>
+    <p>Breed: {{animal.breed}}</p>
   </div>
 </template>
 
 <script>
+  import {mapState} from 'vuex';
+
   export default {
-    name: 'Pet',
+    name: 'Cats',
+    data() {
+      return {
+        animal: {},
+      };
+    },
+    computed: {
+      ...mapState(['cats', 'dogs']),
+    },
+    mounted() {
+      this.animal = this[this.$route.params.species][this.$route.params.id];
+    },
   };
 </script>
 
 <style scoped>
+
 </style>
